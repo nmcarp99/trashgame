e91d32ab6357672a7004b8725da0ee948877e765
nolan <nmcarp99@gmail.com>
2020-02-03 15:58:07 -0500

    Revert "fix dealing cards"
    This reverts commit bdcf755cdbcccf1333e0f0da56d268e5974f74d9.

--- newgame.php
+++ newgame.php
@@ -27,7 +27,7 @@ shuffle($deck);
 for ($i=1;$i<6;$i++) {
 	$myfile = fopen('txt/'.$_COOKIE['game'].$i."useCard.txt", "w") or die("Unable to open file!usecard");
 	for ($j=0;$j<10;$j++) {
-		fwrite($myfile, $deck[0]."\n");
+		fwrite($myfile, $deck[$j*$i]."\n");
 		unset($deck[0]);
 	}
 	fclose($myfile);
