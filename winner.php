85f8631adc464b19eaad2b58a1c9168bd019bdcd
nolan <nmcarp99@gmail.com>
2020-02-02 20:08:54 -0500

    fix more than one player exception for how many discards

--- winner.php
+++ winner.php
@@ -1,3 +1,7 @@
 <?php
-echo json_encode(count(file("txt/".$_COOKIE['game']."discard.txt")));
+if (file("txt/".$_COOKIE['game']."players.txt")[0] == "1") {
+	echo json_encode(count(file("txt/".$_COOKIE['game']."discard.txt")));
+} else {
+	echo json_encode("!1player");
+}
 ?>
